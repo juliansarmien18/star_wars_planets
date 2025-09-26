f3de915f468cb78d01648daf260947ee0a330f9c
Julian Sarmiento <julian010422@gmail.com>
2025-09-26 13:47:51 -0500

    pagination in list

---
+++ core/utils/pagination.py
@@ -0,0 +1,25 @@
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.response import Response
+
+
+class PlanetPagination(PageNumberPagination):
+    """
+    Paginación personalizada para planetas con metadatos completos
+    """
+
+    page_size = 30
+    page_size_query_param = "page_size"
+    max_page_size = 100
+
+    def get_paginated_response(self, data):
+        return Response(
+            {
+                "count": self.page.paginator.count,
+                "next": self.get_next_link(),
+                "previous": self.get_previous_link(),
+                "page_size": self.page_size,
+                "total_pages": self.page.paginator.num_pages,
+                "current_page": self.page.number,
+                "results": data,
+            }
+        )
--- planets/views/planet.py
+++ planets/views/planet.py
@@ -6,15 +6,17 @@ from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
+from core.utils.pagination import PlanetPagination
 from planets.decorators import api_response_handler
 from planets.models import Planet
 from planets.serializers import PlanetSerializer
 
 
 class PlanetViewSet(viewsets.ModelViewSet):
-    queryset = Planet.objects.all()
+    queryset = Planet.objects.all().order_by("name")
     serializer_class = PlanetSerializer
     permission_classes = [IsAuthenticatedOrReadOnly]
+    pagination_class = PlanetPagination
 
     @transaction.atomic
     def perform_create(self, serializer):
